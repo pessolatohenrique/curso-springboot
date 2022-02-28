50e4c1d4396ac9b064833fe8df8cce4255a343c5
pessolatohenrique <pessolatohenrique@gmail.com>
2022-02-28 14:26:16 -0300

    commit inicial

---
+++ src/test/java/br/com/company/cursospring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.company.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
